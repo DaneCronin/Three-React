3f307da21812e947374934ca26052572e763b19f
Dane Cronin <danecronin@gmail.com>
2024-11-12 09:16:26 -0700

    setup React app with Vite and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import './index.css'
+
+
+
+
+const App = () => {
+  return (
+
+    <main>
+      
+    <div>
+      <h1 className="text-3xl font-bold text-gray-400">
+      Hello world!
+    </h1>
+    </div>
+    
+    </main>
+  )
+}
+
+export default App
+
