45b08861775bd6ebd1b5209decd4521bde61065c
Dane Cronin <danecronin@gmail.com>
2024-11-13 16:24:03 -0700

    added functionality to rotate island with mouse movement and arrow keys

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 import planeScene from '../assets/3d/plane.glb'
 import { useGLTF } from '@react-three/drei'
 
-const Plane = () => {
 
-    const  {scene, animations } =useGLTF(planeScene);
+// ...props passes in all the other properties defined and spreads them in setting plane size and position
+const Plane = (isRotating, ...props) => {
+    const ref = useRef();
+
+    const  {scene, animations } = useGLTF(planeScene);
   return (
-  <mesh>
+  <mesh {...props} ref={ref}>
     <primitive object={scene}/>
     </mesh>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState, useEffect, useRef } from 'react'
 import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
 import Island from '../models/Island';
 import Sky from '../models/Sky';
-import Bird from '../models/Bird'
-import Plane from '../models/Plane'
+import Bird from '../models/Bird';
+import Plane from '../models/Plane';
 
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -14,28 +14,57 @@ Popp Up
 
 
 const Home = () => {
+  // New useState to handle rotation of the island
+  const [ isRotating, setIsRotating] = useState(false);
+
+
+
   const adjustIslandForScreenSize = () => {
-    let screenScale= null;
-    let screenPosition = [0, -6.5 , -43];
+    let screenScale, screenPosition;
+
     //define rotation
     let rotation = [0.1, 4.7, 0];
 
     if(window.innerwidth <768) {
       screenScale = [0.9, 0.9, 0.9];
-      
+      screenPosition = [0, -6.5, -43.4];
     } else {
       screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
     }
 //Pass in variables 
      return [screenScale, screenPosition, rotation];
 
-  }
+  };
+
+// Function to adjust plane postioning and scale for screen size
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+   
+    if (window.innerwidth <768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+      
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+//Pass in variables 
+     return [screenScale, screenPosition];
+
+  };
+
+
 // Call AdjustIslandScreenSize function above with properties of scale, position and rotation
 const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
+const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
+
   return (
     <section className = "w-full h-screen relative">
-        <Canvas className="w-full h-screen bg-transparent" 
+      {/* Curly brackets and back ticks to make className dynamic to handle cursor grabbing */}
+        <Canvas className={`w-full h-screen bg-transparent" ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{near: 0.1, far: 1000}}>
           <Suspense fallback={<Loader/>}>
           {/* Directional light simulates light from distant source */}
@@ -65,9 +94,18 @@ const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize(
           position= {islandPosition}
           scale = {islandScale}
           rotation={islandRotation}
+          // Pass isRotating to rotate island with cursor grab
+          isRotating={isRotating}
+          setIsRotating={setIsRotating}
           />
 
-            <Plane/>
+{/* Need to pass in the props of scale and position into Plane  */}
+            <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            isRotating={isRotating}
+            rotation={[0,20,0]}
+              />
 
           </Suspense>
 
