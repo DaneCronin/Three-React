952642f4b14825ea3e38bd6558c678b4ec1934ba
Dane Cronin <danecronin@gmail.com>
2024-11-12 22:13:26 -0700

    added to  pages and Loader component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
+
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+Popp Up
+
+</div> */}
+
 
 const Home = () => {
   return (
     <section className = "w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        Popp Up
+        <Canvas className="w-full h-screen bg-transparent" 
+        camera={{near: 0.1, far: 1000}}>
+          <Suspense fallback={<Loader/>}>
+          <directionalLight/>
+          <ambientLight/>
+          <pointLight/>
+          <spotLight/>
+          <hemisphereLight/>
+          </Suspense>
 
-      </div>
-
+        </Canvas>
 
     </section>
   )
 }
 
-export default Home
+export default Home;
