fddf7e971fd654f4305415a7b07c3f90521975c7
Dane Cronin <danecronin@gmail.com>
2024-11-16 13:22:50 -0700

    added VerticalTimeline component to render experiences

---
+++ src/constants/index.js
@@ -0,0 +1,175 @@
+// import {DaneCroninPhotography, UNCCharlotte, FullCycle  } from "../assets/images";
+import {
+    // ReactMC,
+    contact,
+    css,
+    // PicMe,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    nextjs,
+    nodejs,
+    // ECommerce,
+    react,
+    redux,
+    sass,
+    tailwindcss
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+ 
+];
+
+export const experiences = [
+    {
+        title: "Photographer",
+        company_name: "Dane Cronin Photography",
+        // icon: DaneCroninPhotography,
+        iconBg: "#accbe1",
+        date: "March 2008 - Current",
+        points: [
+            "Owned and operated photography business specializing in cycling photography and interior and architectural design photography.",
+           
+        ],
+    },
+    {
+        title: "Full-Stack Developer Certificate",
+        company_name: "UNC Charlotte",
+        // icon: UNCCharlotte,
+        iconBg: "#fbc3bc",
+        date: "June 2022 - Dec 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with other developers to create high-quality projects.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Buyer",
+        company_name: "Full Cycle",
+        // icon: FullCycle,
+        iconBg: "#b7e4c7",
+        points: [
+            "Worked as a buyer for three store locations and managed all inventory",
+            "Collaborated with owner to setup and establish online store.",
+            
+        ],
+    },
+
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/DaneCronin',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/DaneCronin',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: ECommerce,
+        theme: 'btn-back-red',
+        name: 'ECommerce',
+        description: 'Developed a web application for users to sign up, add items to a shopping cart and checkout using Stripe API.',
+        link: 'https://github.com/DaneCronin/ECommerce',
+    },
+   
+    {
+        iconUrl: MineCraft,
+        theme: 'btn-back-blue',
+        name: 'MineCraft',
+        description: 'Designed and built a social app where users can sign up, build a world in a MineCraft type environment and save a rendering of the world to post and share.',
+        link: 'https://github.com/DaneCronin/ReactMC',
+    },
+   
+    {
+        iconUrl: PicMe,
+        theme: 'btn-back-black',
+        name: 'PicMe',
+        description: 'Developed a web application for users to sign up, login and make posts for other users to like and comment.',
+        link: 'https://github.com/DaneCronin/PicMe',
+    },
+   
+];
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,95 @@
 import React from 'react'
 
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {skills, experiences} from '../constants/index';
+
 const About = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-container">
+      <h1 className="head-text">Hello I'm <span className="blue-gradient_text font-semibold drop-shadow">Dane</span></h1>
+
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>Full-Stack developer based in Colorado</p>
+      </div>
+
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          
+          {/* Map over each skill and for each skill return div with image */}
+          {skills.map((skill => (
+            <div className="block-container w-20 h-20" key={skill.name}>
+              <div className="btn-back rounded-xl"/>
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img src={skill.imageUrl}
+                alt={skill.name}
+                className="w-1/2 h-1/2 object-contain"  />
+                </div>
+            </div>
+          )))}
+          </div>
+           </div>
+
+           {/* Work experience section */}
+<div className="py-16">
+  <h3 className="subhead-text">Work Experience</h3>
+  <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>I've been able to work with many of the top brands in the cycling world as a photographer and am now focusing on building my development skills</p>
+      </div>
+
+      {/* Animated vertical timeline */}
+<div className="mt-12 flex">
+  <VerticalTimeline>
+          {experiences.map((experience) => (
+            <VerticalTimelineElement 
+            key={experience.company_name} 
+            date={experience.date} 
+            icon={
+              <div className='flex justify-center items-center w-full h-full'>
+                <img src={experience.icon} alt={experience.company_name} className="w-[60%] h-[60%] object-contain "/>
+              
+              </div>
+            }  
+            iconStyle={{
+              background: experience.iconBg
+            }}
+           contentStyle= {{
+              borderBottom: '8px',
+              borderStyle: 'solid',
+              borderBottomColor: experience.iconBg,
+              boxShadow: 'none',
+
+              }}>
+
+              <div>
+                <h3 className="font-semibold text-black text-xl font-poppins">{experience.title}</h3>
+                <p className="text-black-500 font-medium font-base" syle={{margin: 0}}>
+                  {experience.company_name}
+                </p>
+              </div>
+
+              <ul className='my-5 list-disc ml-5 space-y-2'>
+                {experience.points.map((point, index) => (
+                    <li key={`experience-point-${index}`} className="text-black-500/50 font-normal pl-1 text-sm">
+                        {point}
+                    
+                    </li>
+                ))}
+              
+              </ul>
+
+            </VerticalTimelineElement>
+
+          ))}
+  </VerticalTimeline>
+
+</div>
+
+</div>
+
+    </section>
+
   )
 }
 
